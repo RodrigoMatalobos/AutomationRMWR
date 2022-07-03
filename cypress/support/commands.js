e0008a56773c8aa66af65086a2e1dabc049ffa51
Rodrigo Matalobos <r.matalobos@mercapsoftware.com>
2022-07-03 20:34:21 -0300

    NewCommand Test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,31 @@
 
 const { randomEmail } = require("./miscellaneous");
 Cypress.Commands.add('randomEmail', randomEmail);
+
+Cypress.Commands.add('signUpUser', (authData) => {
+    cy.readFile('cypress/fixtures/pages/accountCreation.json').then((accountCreation) => {this.accountCreation = accountCreation})
+    cy.readFile(authData).then((dataSigin) => {this.dataSigin = dataSigin})
+
+    cy.get(this.accountCreation.mrRadioButton).should('be.visible').check()
+    cy.get(this.accountCreation.firstNameField).type(this.dataSigin.firstNameField)
+    cy.get(this.accountCreation.lastNameField).type(this.dataSigin.lastNameField)
+    cy.get(this.accountCreation.emailField).should('have.value',this.email.emailField)
+    cy.get(this.accountCreation.passwordField).type(this.dataSigin.passwordField)
+    cy.get(this.accountCreation.dayDropDown).select(this.dataSigin.dayDropDown)
+    cy.get(this.accountCreation.monthDropDown).select(this.dataSigin.monthDropDown)
+    cy.get(this.accountCreation.yearDropDown).select(this.dataSigin.yearDropDown)
+    cy.get(this.accountCreation.addresFirstNameField).should('have.value', this.dataSigin.addresFirstNameField)
+    cy.get(this.accountCreation.addressLastNameField).should('have.value', this.dataSigin.addressLastNameField)
+    cy.get(this.accountCreation.companyField).type(this.dataSigin.companyField)
+    cy.get(this.accountCreation.addressField).type(this.dataSigin.addressField)
+    cy.get(this.accountCreation.addressLine2Field).type(this.dataSigin.addressLine2Field)
+    cy.get(this.accountCreation.cityField).type(this.dataSigin.cityField)
+    cy.get(this.accountCreation.stateDropDown).select(this.dataSigin.stateDropDown)
+    cy.get(this.accountCreation.postCodeField).type(this.dataSigin.postCodeField)
+    cy.get(this.accountCreation.countryDropDown).select(this.dataSigincountryDropDown)
+    cy.get(this.accountCreation.aditionalInformationField).type(this.dataSigin.aditionalInformationField)
+    cy.get(this.accountCreation.homePhoneField).type(this.dataSigin.homePhoneField)
+    cy.get(this.accountCreation.mobilePhoneField).type(this.dataSigin.mobilePhoneField)
+    cy.get(this.accountCreation.aliasAddressField).type(this.dataSigin.aliasAddressField)
+    cy.get(this.accountCreation.registerButton).click()
+});
