7d3d98f0f786ce8e118e99a8cb2709254a15fc43
triipp <walter.rodriguez.dasilva@gmail.com>
2022-07-04 01:18:36 -0300

    fix envio de elementos de accountcreation y apertura de file de datos en subcomando

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,24 +1,23 @@
 
 describe('Sign in automation practice', () => {
   
-	beforeEach(function() {
-      cy.visit('/')
-      cy.readFile('cypress/fixtures/pages/homePage.json').then((homePage) => {this.homePage = homePage})
-      cy.readFile('cypress/fixtures/pages/authentication.json').then((authentication) => {this.authentication = authentication})
-      cy.readFile('cypress/fixtures/pages/accountCreation.json').then((accountCreation) => {this.accountCreation = accountCreation})
-      cy.randomEmail().then((email) => {this.email= email})
-		})
+      beforeEach(function() {
+            cy.visit('/')
+            cy.readFile('cypress/fixtures/pages/homePage.json').then((homePage) => {this.homePage = homePage})
+            cy.readFile('cypress/fixtures/pages/authentication.json').then((authentication) => {this.authentication = authentication})
+            cy.readFile('cypress/fixtures/pages/accountCreation.json').then((accountCreation) => {this.accountCreation = accountCreation})
+            cy.randomEmail().then((email) => {this.email= email})
+	})
   
-  it('Perform a sign in', function() {
-        // Estoy en Homepage
-        cy.get(this.homePage.signInLink).click()
-        // Estoy en authentication page
-        // cy.randomEmail().then(email => { cy.get(this.authentication.emailAddressField).type(email)}) 
-        cy.get(this.authentication.emailAddressField).type(this.email)
-        cy.get(this.authentication.createAnAccountButton).click()
-        // Estoy en accountCreation page
-        cy.signUpUser('cypress/fixtures/data/dataSigin.json')
-        cy.get(this.accountCreation.alertMessage).should('be.visible')
-        cy.get(this.accountCreation.alertMessage).should('contain',"The Zip/Postal code you've entered is invalid. It must follow this format: 00000")
-  })
+      it('Perform a sign in', function() {
+            // Estoy en Homepage
+            cy.get(this.homePage.signInLink).click()
+            // Estoy en authentication page
+            cy.get(this.authentication.emailAddressField).type(this.email)
+            cy.get(this.authentication.createAnAccountButton).click()
+            // Estoy en accountCreation page
+            cy.signUpUser('cypress/fixtures/data/datasignin.json',this.accountCreation)
+            cy.get(this.accountCreation.alertMessage).should('be.visible')
+            cy.get(this.accountCreation.alertMessage).should('contain',"The Zip/Postal code you've entered is invalid. It must follow this format: 00000")
+      })
 })
--- cypress/support/miscellaneous.js
+++ cypress/support/miscellaneous.js
@@ -1,7 +1,8 @@
 
 
-export function randomEmail () {
-    var email = ("test"+(Cypress._.random(0, 1e7)).toString()+"@testautomation.com");
-    cy.log(email);
+export function randomEmail (filename) {
+    var email = ("test"+(Cypress._.random(0, 1e7)).toString()+"@testautomation.com")
+    cy.log(email)
+    cy.writeFile(filename).then((data) => {data.emailField= email})
     return cy.wrap(email);
 }
