6e2a52687cbd343bef3007d56cf8ea387f8be8d1
Rodrigo Matalobos <r.matalobos@mercapsoftware.com>
2022-07-10 19:29:14 -0300

    add mochareports and video and screenshot savers

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -6,7 +6,7 @@ describe('Sign in automation practice', () => {
             cy.readFile('cypress/fixtures/pages/homePage.json').then((homePage) => {this.homePage = homePage})
             cy.readFile('cypress/fixtures/pages/authentication.json').then((authentication) => {this.authentication = authentication})
             cy.readFile('cypress/fixtures/pages/accountCreation.json').then((accountCreation) => {this.accountCreation = accountCreation})
-            cy.randomEmail('cypress/fixtures/data/dataSigin.json').then((email) => {this.email= email})
+            cy.randomEmail('cypress/fixtures/data/dataSignIn.json').then((email) => {this.email= email})
 	})
   
       it('Perform a sign in', function() {
